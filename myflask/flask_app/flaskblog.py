d53a0d0b54aff5c151f9a870f594de499976e583
GokilaVenugopal <46557917+GokilaVenugopal@users.noreply.github.com>
2020-07-26 10:37:40 +0530

    Update flaskblog.py

--- myflask/flask_app/flaskblog.py
+++ myflask/flask_app/flaskblog.py
@@ -35,14 +35,14 @@ class Post(db.Model):
 
 posts = [
     {
-        'author': 'Corey Schafer',
-        'title': 'Blog Post 1',
+        'author': 'Artificial Intelligence',
+        'title': 'Session 1',
         'content': 'First post content',
         'date_posted': 'April 20, 2018'
     },
     {
-        'author': 'Jane Doe',
-        'title': 'Blog Post 2',
+        'author': 'Data Structure',
+        'title': 'Session 2',
         'content': 'Second post content',
         'date_posted': 'April 21, 2018'
     }
